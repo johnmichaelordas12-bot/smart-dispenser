8af62eb20f76e7fe77187d309146d8fffe98d870
John Michael Ordas <johnmichaelordas12@gmail.com>
2026-03-23 13:05:13 +0800

    added database config

--- config.py
+++ config.py
@@ -1,10 +1,25 @@
+import os
+
+# ==============================
+# SYSTEM SETTINGS
+# ==============================
 API_KEY = "SMARTDISPENSER12345"
 SERVER_PORT = 5000
 
-# Slot configuration (scalable design)
-DEFAULT_SLOTS = 14   # current physical dispenser
-MAX_SLOTS = 50       # future expansion
+DEFAULT_SLOTS = 14
+MAX_SLOTS = 50
 
+# ==============================
+# EMAIL CONFIG
+# ==============================
 GMAIL_SENDER = "smartdispenser.system@gmail.com"
-GMAIL_APP_PASSWORD = "bncq nmgg nlqe jymo"   # app password
+GMAIL_APP_PASSWORD = "bncq nmgg nlqe jymo"
 
+# ==============================
+# DATABASE CONFIG (RAILWAY)
+# ==============================
+MYSQL_HOST = os.getenv("MYSQLHOST")
+MYSQL_USER = os.getenv("MYSQLUSER")
+MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
+MYSQL_DB = os.getenv("MYSQLDATABASE")
+MYSQL_PORT = int(os.getenv("MYSQLPORT", 3306))
